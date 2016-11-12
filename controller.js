4ff81b0e705022f2258319321ce145904055b499
Kyle Barlow <kbarlow@kbarlow-l2.corp.instructure.com>
2016-11-11 22:17:10 -0700

    set up saving to local storage

--- controller.js
+++ controller.js
@@ -1,16 +1,52 @@
 angular.module('app')
     .controller("mainCtrl", function($scope) {
-
         $scope.supported = false;
+        $scope.startFollowUpButton = false
 
-        $scope.nameToCopy = "";
-        $scope.emailToCopy = ""
+        $scope.clearLocalStorage = function() {
+            localStorage.clear();
+            $scope.startFollowUpButton = false;
+            location.reload();
+        }
 
-        $scope.success = function () {
+        $scope.followUpInputContainer = true;
+        if (localStorage.getItem('session')) {
+            $scope.followUpInputContainer = false;
+            $scope.startFollowUpButton = true;
+        }
+        $scope.startFollowUps = function() {
+            var a = [];
+            a.push(JSON.parse(localStorage.getItem('session')));
+            localStorage.setItem('session', JSON.stringify(a));
+            $scope.followUpInputContainer = false;
+            $scope.startFollowUpButton = true;
+        }
+        $scope.addToFollowups = function(name, email, notes) {
+                var followUp = {
+                    name: name,
+                    email: email,
+                    notes: notes
+                }
+                var a = [];
+                // Parse the serialized data back into an aray of objects
+                a = JSON.parse(localStorage.getItem('session'));
+                // Push the new data (whether it be an object or anything else) onto the array
+                a.push(followUp);
+                // Re-serialize the array back into a string and store it in localStorage
+                localStorage.setItem('session', JSON.stringify(a));
+                $scope.nameToCopy1 = "";
+                $scope.emailToCopy1 = "";
+                $scope.notesToCopy1 = "";
+                location.reload();
+        }
+
+        $scope.followUps = JSON.parse(localStorage.getItem("session"));
+
+        $scope.success = function() {
             console.log('Copied!');
         };
 
-        $scope.fail = function (err) {
+        $scope.fail = function(err) {
             console.error('Error!', err);
         };
 
